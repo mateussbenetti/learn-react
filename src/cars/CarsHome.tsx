4d78a61a3ef4062acf08f76c6c3820fd3ddb5156
Mateus de Oliveira Benetti <mateusdeoliveirabenetti@gmail.com>
2024-10-30 21:09:36 -0300

    create carswhatweoffers

--- src/cars/CarsHome.tsx
+++ src/cars/CarsHome.tsx
@@ -3,15 +3,17 @@ import CarsHeader from "./CarsHeader";
 import CarsOurAdvantages from "./CarsOurAdvantages";
 import CarsTopBrands from "./CarsTopBrands";
 import CarsTopCategories from "./CarsTopCategories";
+import CarsWhatWeOffer from "./CarsWhatWeOffer";
 
 export default function CarsHome() {
   return (
     <div>
-      {/* <CarsHeader />
+      <CarsHeader />
       <CarsCard />
       <CarsTopBrands />
-      <CarsTopCategories /> */}
+      <CarsTopCategories />
       <CarsOurAdvantages />
+      <CarsWhatWeOffer />
     </div>
   );
 }
---
+++ src/cars/CarsWhatWeOffer.tsx
@@ -0,0 +1,108 @@
+import {
+  faBrush,
+  faFile,
+  faScrewdriverWrench,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function CarsWhatWeOffer() {
+  return (
+    <div className="font-inter p-4 gap-6">
+      <div className="flex justify-between">
+        <div className="font-bold text-lg">What We Offer</div>
+      </div>
+      <div className="text-gray-400 text-sm">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s
+      </div>
+      <div className="inline-block h-[290px] w-full max-w-screen-lg shadow-sm shadow-gray-500 rounded-md mt-4">
+        <div className="flex h-[165px] bg-gray-300 rounded-t-lg text-center justify-center items-center">
+          photo
+        </div>
+        <div className="h-[128px]">
+          <div className="flex font-inter justify-between text-center items-center p-2">
+            <div className="text-blue-600 font-bold text-xl">€ 25</div>
+          </div>
+          <div className="px-2">
+            <h1 className="font-bold text-base">COMPLETE VEHICLE WASHING</h1>
+            <p className="text-gray-400 text-sm">
+              Lorem ipsum dolor sit amet pretium consectetur adipiscing elit.
+              Lorem consectetur adipiscing elit.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="inline-block h-[290px] w-full max-w-screen-lg shadow-sm shadow-gray-500 rounded-md mt-4">
+        <div className="flex h-[165px] bg-gray-300 rounded-t-lg text-center justify-center items-center">
+          photo
+        </div>
+        <div className="h-[128px]">
+          <div className="flex font-inter justify-between text-center items-center p-2">
+            <div className="text-blue-600 font-bold text-xl">€ 25</div>
+          </div>
+          <div className="px-2">
+            <h1 className="font-bold text-base">
+              IRONING AND PAINTING WORKSHOP
+            </h1>
+            <p className="text-gray-400 text-sm">
+              Lorem ipsum dolor sit amet pretium consectetur adipiscing elit.
+              Lorem consectetur adipiscing elit.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="inline-block h-[290px] w-full max-w-screen-lg shadow-sm shadow-gray-500 rounded-md mt-4">
+        <div className="flex h-[165px] bg-gray-300 rounded-t-lg text-center justify-center items-center">
+          photo
+        </div>
+        <div className="h-[128px]">
+          <div className="flex font-inter justify-between text-center items-center p-2">
+            <div className="text-blue-600 font-bold text-xl">€ 25</div>
+          </div>
+          <div className="px-2">
+            <h1 className="font-bold text-base">INSURANCE AGENTS</h1>
+            <p className="text-gray-400 text-sm">
+              Lorem ipsum dolor sit amet pretium consectetur adipiscing elit.
+              Lorem consectetur adipiscing elit.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="inline-block h-[290px] w-full max-w-screen-lg shadow-sm shadow-gray-500 rounded-md mt-4">
+        <div className="flex h-[165px] bg-gray-300 rounded-t-lg text-center justify-center items-center">
+          photo
+        </div>
+        <div className="h-[128px]">
+          <div className="flex font-inter justify-between text-center items-center p-2">
+            <div className="text-blue-600 font-bold text-xl">€ 25</div>
+          </div>
+          <div className="px-2">
+            <h1 className="font-bold text-base">INSURANCE AGENTS</h1>
+            <p className="text-gray-400 text-sm">
+              Lorem ipsum dolor sit amet pretium consectetur adipiscing elit.
+              Lorem consectetur adipiscing elit.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="inline-block h-[290px] w-full max-w-screen-lg shadow-sm shadow-gray-500 rounded-md mt-4">
+        <div className="flex h-[165px] bg-gray-300 rounded-t-lg text-center justify-center items-center">
+          photo
+        </div>
+        <div className="h-[128px]">
+          <div className="flex font-inter justify-between text-center items-center p-2">
+            <div className="text-blue-600 font-bold text-xl">€ 25</div>
+          </div>
+          <div className="px-2">
+            <h1 className="font-bold text-base">INSURANCE AGENTS</h1>
+            <p className="text-gray-400 text-sm">
+              Lorem ipsum dolor sit amet pretium consectetur adipiscing elit.
+              Lorem consectetur adipiscing elit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
